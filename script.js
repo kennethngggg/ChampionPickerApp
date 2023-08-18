b2fd6fd07688030d0f7157f2ae1ee816faa12514
kennethngggg <142420134+kennethngggg@users.noreply.github.com>
2023-08-18 19:21:00 +0800

    Full data + first working protype

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 let questions = [];
 let champions = [];
+let significanceFactors = []; // <-- add this
 let currentQuestionIndex = 0;
 
+
 async function loadInitialData() {
-    await fetchData();
-    displayQuestion();
+    try {
+        await fetchChampionData();
+        displayQuestion();
+    } catch (err) {
+        console.error("Error fetching data:", err);
+        // Here, you can also add some code to display an error message on the webpage, if needed.
+    }
 }
 
-async function fetchData() {
-    const response = await fetch('championData.csv');
-    const data = await response.text();
-    const rows = data.split('\n');
-    
-    // Extracting questions
-    questions = rows[0].split(',').slice(1);
+async function fetchChampionData() {
+    const response = await fetch('championData.xlsx');
+    const arrayBuffer = await response.arrayBuffer();
+    const data = new Uint8Array(arrayBuffer);
+    const workbook = XLSX.read(data, { type: "array" });
 
-    // Extracting champion data
-    champions = rows.slice(1).map(row => {
-        const columns = row.split(',');
+    // Assuming data is in the first worksheet
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
+
+    // Convert worksheet to JSON
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    // Extract questions, significance factors, and champion data
+    questions = jsonData[0].slice(1); // because first cell is probably empty or has a header like "Champions"
+    
+    significanceFactors = jsonData[1].slice(1).map(Number);
+
+    champions = jsonData.slice(2).map(row => { 
         return {
-            name: columns[0],
-            weights: columns.slice(1).map(Number)
+            name: row[0],
+            weights: row.slice(1).map(Number),
+            score: 0  // Initialize score here
         };
     });
 }
@@ -38,28 +54,56 @@ function endQuiz() {
     displayFinalChampionRecommendation();
 }
 
-function handleOptionClick(option) {
-    adjustChampionScores(option);
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-        displayQuestion();
-        displayDebugChampionRecommendation();
-    } else {
-        endQuiz();
-    }
-}
-
 function adjustChampionScores(option) {
     champions.forEach(champion => {
         let weight = champion.weights[currentQuestionIndex];
-        
-        if (option === "Yes") weight *= 3;
-        else if (option === "Maybe") weight *= 2;
-        else if (option === "No") weight *= -1;
-        
-        if (!champion.score) champion.score = 0;
+        const significance = significanceFactors[currentQuestionIndex];
+
+        switch(option) {
+            case "Yes":
+                weight *= significance;  // Multiply by significance
+                break;
+            case "Maybe":
+                weight *= (0.5 * significance);  // Reduce impact based on significance
+                break;
+            case "No":
+                weight *= (-1 * significance);  // Negate and multiply by significance
+                break;
+        }
         champion.score += weight;
     });
+}
+
+
+const STRONG_PREFERENCE_THRESHOLD = 15; // Adjust this threshold as needed.
+
+function hasStrongChampionPreference() {
+    // Sort champions without modifying the original array.
+    const sortedChampions = [...champions].sort((a, b) => b.score - a.score);
+
+    // Check if the top champion has a score differential greater than the threshold.
+    return (sortedChampions[0].score - sortedChampions[1].score) > STRONG_PREFERENCE_THRESHOLD;
+}
+
+function handleOptionClick(option) {
+    adjustChampionScores(option);
+    currentQuestionIndex++;
+
+    if (hasStrongChampionPreference() || currentQuestionIndex >= questions.length) {
+        endQuiz();
+    } else {
+        displayQuestion();
+        displayDebugChampionRecommendation();
+    }
+}
+
+const LOW_VARIANCE_THRESHOLD = 5; // Adjust this threshold as needed.
+
+function calculateStandardDeviation() {
+    const n = champions.length;
+    const mean = champions.reduce((acc, champ) => acc + champ.score, 0) / n;
+    const variance = champions.reduce((acc, champ) => acc + Math.pow(champ.score - mean, 2), 0) / n;
+    return Math.sqrt(variance);
 }
 
 function displayDebugChampionRecommendation() {
@@ -76,6 +120,15 @@ function displayDebugChampionRecommendation() {
 function displayFinalChampionRecommendation() {
     const finalList = document.getElementById('finalChampionList');
     finalList.innerHTML = '';
+
+    const stdDev = calculateStandardDeviation();
+    if (stdDev < LOW_VARIANCE_THRESHOLD) {
+        // Show a message about the neutral preference.
+        const neutralMessage = document.createElement('p');
+        neutralMessage.textContent = "Your answers suggest a neutral preference. Here are some general champion recommendations:";
+        finalList.appendChild(neutralMessage);
+    }
+
     const topChampions = getTopChampions(5);
     topChampions.forEach(champion => {
         const listItem = document.createElement('li');
@@ -83,7 +136,6 @@ function displayFinalChampionRecommendation() {
         finalList.appendChild(listItem);
     });
 }
-
 
 function getTopChampions(topN) {
     champions.sort((a, b) => b.score - a.score);
